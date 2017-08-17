29b135bf55ce00a0a498703f842490202bd756fd
mquinteiro hp-home <mquinteiro@gmail.com>
2017-08-17 13:35:44 +0200

    starting with video

--- search_cars.py
+++ search_cars.py
@@ -11,17 +11,17 @@ import cv2
 orient = 9
 pix_per_cell = 8
 cell_per_block = 2
+spatial_size = 16
+hist_bins = 32
 #spatial_size = dist_pickle["spatial_size"]
 #hist_bins = dist_pickle["hist_bins"]
-
-img = cv2.imread('test_image.jpg')
 
 
 # Define a single function that can extract features using hog sub-sampling and make predictions
 def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
     draw_img = np.copy(img)
-    img = img.astype(np.float32) / 255
-
+    #img = img.astype(np.float32) / 255 # I dont change to (0,255) because it is normailzed by scaler
+    rectangles = []
     img_tosearch = img[ystart:ystop, :, :]
     ctrans_tosearch = cv2.cvtColor(img_tosearch,cv2.COLOR_BGR2YCrCb)
     if scale != 1:
@@ -66,30 +66,70 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
             subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
 
             # Get color features
-            spatial_features = bin_spatial(subimg, size=spatial_size)
+            spatial_features = bin_spatial(subimg, size=(spatial_size,spatial_size))
             hist_features = color_hist(subimg, nbins=hist_bins)
 
             # Scale features and make a prediction
             test_features = X_scaler.transform(
-                np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
+                np.hstack((hog_features, spatial_features, hist_features)).reshape(1, -1))
             # test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))
             test_prediction = svc.predict(test_features)
-
+            #test_prediction=1
             if test_prediction == 1:
                 xbox_left = np.int(xleft * scale)
                 ytop_draw = np.int(ytop * scale)
                 win_draw = np.int(window * scale)
-                cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
-                              (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 6)
+                rectangles.append([(xbox_left, ytop_draw + ystart),
+                              (xbox_left + win_draw, ytop_draw + win_draw + ystart)])
+    return rectangles
+    #return draw_img
 
-    return draw_img
+def search_cars(img,areas):
+    rectangles=[]
+    for area in areas:
+        rc = find_cars(img, area[0], area[1], area[2], svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
+                       hist_bins)
+        if len(rc) > 0:
+            if len(rectangles) > 0:
+                rectangles = np.vstack((rectangles, rc))
+            else:
+                rectangles = rc
+    return rectangles
 
 
-ystart = 400
-ystop = 656
-scale = 1.5
+for i in [1,2,3,4,5,6]:
+    
+    img = cv2.imread('test_images/test'+str(i)+'.jpg')
+    areas = [[400,656,2],[400,625,1.5],[400,550,1.2],[400,500,1],[400,450,0.8]]
+    heat = np.zeros_like(img[:, :, 0]).astype(np.float)
+    rectangles = search_cars(img,areas)
+    out_img = img.copy()
+    if len(rectangles) > 0:
+        heat = add_heat(heat, rectangles)
+        heat = apply_threshold(heat, 1)
+        heatmap = np.clip(heat, 0, 255)
+        labels = label(heatmap)
+        draw_img = draw_labeled_bboxes(np.copy(img), labels)
+        cv2.imshow("ventana", draw_img)
+        cv2.waitKey(1)
+        fig = plt.figure()
+        plt.subplot(121)
+        plt.imshow(draw_img)
+        plt.title('Car Positions')
+        plt.subplot(122)
+        plt.imshow(heatmap, cmap='hot')
+        plt.title('Heat Map')
+        fig.tight_layout()
 
-out_img = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
-                    hist_bins)
 
-plt.imshow(out_img)
+
+    #        for rec in rectangles:
+    #            cv2.rectangle(out_img, tuple(rec[0]),
+    #                      tuple(rec[1]), (0, 0, 255), 6)
+
+    #plt.imshow(out_img)
+    #cv2.imshow("ventana",out_img)
+    #    if i==1:
+    #        cv2.imwrite("mulpiple_sizes_detection.jpg",out_img)
+    #cv2.waitKey(1)
+pass
